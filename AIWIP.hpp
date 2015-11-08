369c035d3b1f1815c90d35151f94f570027494aa
Eric Hsin <eris.hsin007@gmail.com>
2015-11-07 22:51:58 -0800

    AIWIP code fixed not calling eval function

--- AIWIP.hpp
+++ AIWIP.hpp
@@ -19,32 +19,32 @@ public:
     AIWIP(const char* name) : AIShell(name)
     {}
 protected:
-    
+
     virtual void _boardPopulated()
     {
         buildScoring();
     }
     void moveToFront(GameNode** old_head, GameNode** new_head)
     {
-        if(old_head==new_head)
+        if (old_head == new_head)
             return;
         GameNode* hold = *new_head;
-        *new_head=hold->next;
-        hold->next=*old_head;
+        *new_head = hold->next;
+        hold->next = *old_head;
         *old_head = hold;
     }
     int8_t ids(int16_t alpha, int16_t beta, GameNode** root, GameBoard board, unsigned int remaining_depth, cellType turn)
     {
         int16_t best;
         GameNode** walker = root;
-        unsigned int next_depth = remaining_depth-1;
-        cellType next_turn = (cellType)(turn^1);
-        bool our_turn = turn&1;
+        unsigned int next_depth = remaining_depth - 1;
+        cellType next_turn = (cellType)(turn ^ 1);
+        bool our_turn = turn & 1;
         int16_t val;
-        if(!*root)
+        if (!*root)
         {
-            *root = board.getPossibleMoves(_num_col,_num_row);
-            if(our_turn)
+            *root = board.getPossibleMoves(_num_col, _num_row);
+            if (our_turn)
                 best = INT16_MIN;
             else
                 best = INT16_MIN;
@@ -52,30 +52,32 @@ protected:
         else
             best = (*root)->value;
 
-        while(*walker)
+        while (*walker)
         {
-            if(!_run)
+            if (!_run)
                 throw 1;
-            board.addMove((*walker)->my_move,turn);
-            if(next_depth)
+            board.addMove((*walker)->my_move, turn);
+            if (next_depth)
                 val = ids(alpha, beta, &(*walker)->child, board, next_depth, next_turn);
             else
-                val = eval(board,our_turn);
-            
-            (*walker)->value = val;
-            if(our_turn)
             {
-                if(val >= beta)
+                val = eval(board, our_turn);
+            }
+
+            (*walker)->value = val;
+            if (our_turn)
+            {
+                if (val >= beta)
                     return val;
-                if(val > best)
-                    moveToFront(root,walker);
+                if (val > best)
+                    moveToFront(root, walker);
             }
             else
             {
-                if(val <= alpha)
+                if (val <= alpha)
                     return val;
-                if(val < best)
-                    moveToFront(root,walker);
+                if (val < best)
+                    moveToFront(root, walker);
             }
             board.removeMove((*walker)->my_move);
             walker = &(*walker)->next;
@@ -84,14 +86,14 @@ protected:
     }
     virtual void _logic(unsigned int target_depth)
     {
-        try{
-            ids(INT16_MIN, INT16_MAX, &_root, _game, target_depth, cellType(2|(_move_count&1)));
-            _move=_root->my_move;
+        try {
+            ids(INT16_MIN, INT16_MAX, &_root, _game, target_depth, cellType(2 | (_move_count & 1)));
+            _move = _root->my_move;
         }
         catch (...)
         {}
-        
-        
+
+
         /* iterative may be attempted later...
         GameNode** walker = &_root;
         while(_run)
@@ -110,36 +112,36 @@ protected:
                     if(turn==cellType::US)
                     {
                         // Alpha/beta pruning goes here
-                        
+
                     }
                 }
             }
             else if(depth<target_depth)
             {
-                
+
             }
         }
          */
     }
     virtual void _cleanTree()
     {
-        if(!_root)
+        if (!_root)
             return;
         GameNode* walker = _root;
         GameNode* hold = nullptr;
-        while(walker->next && walker->next->my_move != _move)
+        while (walker->next && walker->next->my_move != _move)
             walker = walker->next;
-        if(walker->next)
+        if (walker->next)
         {
             hold = walker->child;
             walker->child = nullptr;
         }
-        
+
         delete _root;
         _root = hold;
     }
     GameNode *_root;
-    
+
     /* what is in AIShell.h:
      std::string _name;
      std::thread* _builder;
@@ -154,37 +156,52 @@ protected:
      */
 
     //heuristic stuff:
-    
+
     int *_row; //socring for thw row
     int *_column;  //socring for the column
     int **_diagonal;
-    
-    
+
+
     int calculateMax(uint8_t number)   {
         return (number - _k + 1);
     }
-    
+
     //this only need to be called once per game
     void buildScoring() {
+        _row = new int [_num_col] ();
+        _column = new int [_num_row]();
+        _diagonal = new int*[_num_col];
+        for (int i = 0; i < _num_col; i++) {
+            _diagonal[i] = new int[_num_row]();
+        }
+        _row = new int [_num_col] ();
+        _column = new int [_num_row]();
+        _diagonal = new int*[_num_col];
+        for (int i = 0; i < _num_col; i++) {
+            _diagonal[i] = new int[_num_row]();
+        }
+
+
         int col_number = calculateMax(_num_col);
         int row_number = calculateMax(_num_row);
         int temp = 0;
         buildHelper(_column, col_number, _num_col);
         buildHelper(_row, row_number, _num_row);
         diagHelper();
-        
+
+
         std::cout << "row: " << std::endl;
         for (int i = 0; i < _num_row; i++) {
             std::cout << _row[i];
         }
         std::cout << std::endl;
-        
+
         std::cout << "column: " << std::endl;
         for (int i = 0; i < _num_col; i++) {
             std::cout << _column[i];
         }
         std::cout << std::endl;
-        
+
         std::cout << "diagonal: " << std::endl;
         for (int col = 0; col < _num_col; col++) {
             for (int row = 0; row < _num_row; row++) {
@@ -193,19 +210,19 @@ protected:
             std::cout << std::endl;
         }
     }
-    
+
     void diagHelper() {
         int ** temp = new int* [_k];
         for (int i = 0; i < _k; i++) {
             temp[i] = new int[_k]();
         }
-        
+
         for (int i = 0; i < _k; i++) {
             temp[i][i] = temp[i][i] + 1;
             temp[i][_k - i - 1] = temp[i][_k - i - 1] + 1 ;
         }
         diagApply(temp);
-        
+
     }
     void diagApply(int **kboard) {
         for (int boardCol = 0; boardCol < _num_col - _k + 1; boardCol++) {
@@ -217,10 +234,10 @@ protected:
             }
         }
     }
-    
+
     void buildHelper(int *array, int maxium, int size) {
         bool flag = _k < maxium;
-        
+
         int changeIndex = std::max(maxium, (int)_k);
         //ture change from max
         if (flag) {
@@ -238,7 +255,7 @@ protected:
             for (; i < size; i++) {
                 array[i] = temp--;
             }
-            
+
         }
         //false chage from _k
         else {
@@ -258,8 +275,8 @@ protected:
             }
         }
     }
-    
-    
+
+
     int goalTest(const GameBoard& board) {
         int goal = 0;
         if (_k == 2)
@@ -268,13 +285,15 @@ protected:
             goal = 2;
         else
             goal = _k - 1;
-        
+
         //0 no wining move on us or the enemy
         //1 we are wining
         //-1 we are going to lose
         int vscore = 0;
-        
+
+        //column
         for (int i = 0; i < _num_col; i++) {
+            //row
             for (int j = 0; j < _num_row; j++) {
                 if (std::abs(vscore) == goal)
                     return vscore;
@@ -296,9 +315,11 @@ protected:
                 }
             }
         }
-        
+
         int hscore = 0;
+        //row
         for (int i = 0; i < _num_row; i++) {
+            //column
             for (int j = 0; j < _num_col ; j++) {
                 if (std::abs(hscore) == goal)
                     return hscore;
@@ -320,13 +341,14 @@ protected:
                 }
             }
         }
-        
+
         return 0;
-        
+
     }
-    
+
     //call this to eval the gameboard everything else I added are just to build scoring sheets
     int eval(const GameBoard& board, bool our_turn)    {
+        std::cout << "start eval" << std::endl;
         int AIscore = 0;
         int HMscore = 0;
         //i is column
@@ -343,8 +365,8 @@ protected:
         }
         return AIscore - HMscore + 100 * goalTest(board);
     }
-    
-    
+
+
 };
 
 #endif /* AIWIP_hpp */
